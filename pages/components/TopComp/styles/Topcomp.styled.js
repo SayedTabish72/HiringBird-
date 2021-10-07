2cdfce1ac29b9f43ea4513307f6b5fa1a0e144ad
aasim-syed <70385414+aasim-syed@users.noreply.github.com>
2021-10-07 23:39:04 +0530

    Add files via upload

---
+++ pages/components/TopComp/styles/Topcomp.styled.js
@@ -0,0 +1,265 @@
+import { ECHILD } from "constants";
+import { ECDH } from "crypto";
+import styled from "styled-components";
+import pp1 from '../../../public/paypal.png'
+import pp2 from '../../../public/paypal.png'
+import sl1 from '../../../public/slack.png'
+import sl2 from '../../../public/slack.png'
+import monke1 from '../../../public/monke.png'
+import monke2 from '../../../public/monke.png'
+import mc1 from '../../../public/mc.png'
+import mc2 from '../../../public/mc.png'
+import dp1 from '../../../public/dropbox.png'
+import dp2 from '../../../public/dropbox.png'
+import bl1 from '../../../public/bluelogo.png'
+import bl2 from '../../../public/bluelogo.png'
+export const TopCom = styled.div`
+
+width: 1005px;
+height: 98px;
+margin-left: 162px;
+
+margin-top:68px;
+font-family: Inter;
+font-style: normal;
+font-weight: 800px;
+font-size: 60px;
+line-height: 98px;
+/* identical to box height, or 136% */
+
+font-feature-settings: 'salt' on, 'liga' off;
+color: #090d33;
+
+
+`;
+export const Purple = styled.div`
+
+position: absolute;
+width: 100%;
+
+left: 0px;
+top: 6618px;
+right: 0px;
+
+background: #C9CBE2;
+
+
+
+    height: 75vh;
+
+    @media (max-width: 1200px) {
+ 
+        margin-left: 0px;
+        padding-right: 1200px;
+        
+      
+  }
+  @media (max-width: 1024px) {
+ 
+    margin-left: 0px;
+    padding-right: 1200px;
+    
+  
+}
+@media (max-width: 540px) {
+ 
+    margin-left: 0px;
+    
+    
+  
+}
+@media (max-width: 700px) {
+ 
+      margin-left: 0px;
+      padding-right: 2000px;
+      
+    
+}
+@media screen (max-width: 360px) {
+   
+      margin-left: 0px;
+      padding-right: 1240px;
+      padding-bottom: 600px;
+ 
+
+}
+@media (max-width: 380px){
+  
+      
+      margin-left: -30px;
+      padding-right: 1200px;
+      padding-bottom: 600px;
+  
+
+}
+
+
+
+
+`;
+export const Red = styled.div`
+
+position: static;
+width: 445px;
+height: 82px;
+left: 0px;
+margin-top: 30px;
+
+font-family: Inter;
+font-style: normal;
+font-weight: 800;
+font-size: 36px;
+line-height: 41px;
+/* or 114% */
+
+
+color: #F26A7E;
+
+
+
+
+
+
+`;
+export const Belowred = styled.div`
+
+position: absolute;
+width: 565px;
+height: 32px;
+left: 162px;
+top: 339px;
+
+font-family: Inter;
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 32px;
+/* identical to box height, or 160% */
+font-feature-settings: 'salt' on, 'liga' off;
+
+color: #404366;
+
+`;
+export const Paypal1= styled.img`
+
+width: 81.57px;
+height: 81.57px;
+margin-left: 900px;
+margin-top: -2em;
+
+filter: drop-shadow(0px 9.30838px 18.6168px rgba(41, 41, 42, 0.07));
+
+
+
+
+
+`;
+export const Paypal2 = styled.img`
+
+width: 81.57px;
+height: 81.57px;
+margin-left: 900px;
+margin-top: 122px;
+
+
+filter: drop-shadow(0px 9.30838px 18.6168px rgba(41, 41, 42, 0.07));
+
+`;
+export const Mc1 =styled.img`
+
+width: 81.57px;
+height: 81.57px;
+margin-left: 1039px;
+margin-top: -5em;
+
+
+filter: drop-shadow(0px 9.30838px 18.6168px rgba(41, 41, 42, 0.07));
+
+
+
+`;
+export const Mc2 = styled.img`
+
+width: 81.57px;
+    height: 81.57px;
+    margin-left: 1039px;
+    margin-top: 2.2em;
+    
+    
+    filter: drop-shadow(0px 9.30838px 18.6168px rgba(41, 41, 42, 0.07));
+
+
+
+
+`;
+
+export const Dp1 = styled.img`
+
+width: 81.57px;
+    height: 81.57px;
+    margin-left: 1139px;
+    margin-top: -1.41em;
+    
+    
+    filter: drop-shadow(0px 9.30838px 18.6168px rgba(41, 41, 42, 0.07));
+
+
+
+
+`;
+export const Monke1 = styled.img`
+
+
+
+width: 81.57px;
+    height: 81.57px;
+    margin-left: 1039px;
+    margin-top: -1.41em;
+    
+    
+    filter: drop-shadow(0px 9.30838px 18.6168px rgba(41, 41, 42, 0.07));
+
+`;
+export const Bl1 =styled.img`
+width: 81.57px;
+height: 81.57px;
+margin-left: 839px;
+margin-top: -1.2em;
+
+
+filter: drop-shadow(0px 9.30838px 18.6168px rgba(41, 41, 42, 0.07));
+
+
+
+
+`;
+export const Bl2 = styled.img`
+
+
+
+
+
+
+`;
+export const Join =styled.button`
+
+
+/* Auto Layout */
+display: flex;
+flex-direction: row;
+align-items: flex-start;
+padding: 17px 35px;
+
+position: absolute;
+width: 161px;
+height: 53px;
+left: 156px;
+top: 393px;
+border-style: none;
+background: #F26A7E;
+border-radius: 6px;
+font-family: Inter;
+color: white;
+/* label */
+
+`;
