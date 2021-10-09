7a9ce35042300ccf90431f74ca016f9d6e60a604
anand <anandca.an7@gmail.com>
2021-10-09 12:20:50 +0530

    ssr added

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html>
@@ -9,6 +21,8 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
           />
+          {/* Step 5: Output the styles in the head  */}
+          {this.props.styleTags}
         </Head>
         <body>
           <Main />
@@ -18,5 +32,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
