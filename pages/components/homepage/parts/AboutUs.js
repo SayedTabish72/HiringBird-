67bb1c54974a96eaf436a4302856f03bcfd3e8db
Kartik Soni <kartiksoni2209@gmail.com>
2021-10-12 18:49:07 +0530

    about us and foundes note sections with responsiveness

---
+++ pages/components/homepage/parts/AboutUs.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Aboutuscard } from './styles/Aboutus.styled'
+
+
+const AboutUs = () => {
+    return (
+        <Aboutuscard>
+            <div>
+                <h2>About Us</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer dolor sit amet, consectetur adipiscing elit. Nunc odio in et. id integer dolor sit amet, consectetur adipiscing elit. Net.
+                </p>
+            </div>
+            <div>
+                <img src = '../../../../aboutus.png'></img>
+            </div>
+        </Aboutuscard>
+    )
+}
+
+export default AboutUs
